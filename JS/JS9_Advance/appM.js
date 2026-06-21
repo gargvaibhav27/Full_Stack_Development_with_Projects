ae9cff0aa313a478fc1bda8a356a98b655a87b57
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-06-21 15:10:20 +0530

    importing

---
+++ JS/JS9_Advance/appM.js
@@ -0,0 +1,13 @@
+// default import
+
+import multiply from "./mathOperationsM.js";
+
+// named import
+
+import {add, subtract} from "./mathOperationsM.js";
+
+console.log(multiply(2,2));
+console.log(add(2,4));
+console.log(subtract(4,2));
+
+
