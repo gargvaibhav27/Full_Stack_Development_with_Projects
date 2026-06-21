6fa1f813e11eca92bf7e97ad267a8abafdda3fee
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-06-21 13:17:51 +0530

    promises

---
+++ JS/JS9_Advance/promises.js
@@ -0,0 +1,28 @@
+// promise in JavaScript is an object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value. A Promise is in one of these states:
+// pending: initial state, neither fulfilled nor rejected.
+// fulfilled: meaning that the operation completed successfully.
+// rejected: meaning that the operation failed.
+
+function fetchData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let sucess = true; // change this to false to see the rejected state
+      if (sucess) {
+        resolve("Data fetched successfully");
+      } else {
+        reject("Error fetching data");
+      }
+    }, 3000);
+  });
+}
+// let response = fetchData();
+// console.log(response); // Promise {<pending>}
+//here we are getting the promise object in pending state because the fetchData function is asynchronous and it takes 3 seconds to complete. So, the promise is in pending state for 3 seconds and then it will be either fulfilled or rejected based on the value of the success variable.
+
+fetchData()
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
