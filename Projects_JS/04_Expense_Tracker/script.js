a579a0fbe3404662c6ab4ddd16c65df003104651
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-06-27 14:08:19 +0530

    expense tracker

---
+++ Projects_JS/04_Expense_Tracker/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const expenseForm = document.getElementById("expense-form");
+  const expenseNameInput = document.getElementById("expense-name");
+  const expenseAmountInput = document.getElementById("expense-amount");
+  const expenseList = document.getElementById("expense-list");
+  //const total = document.getElementById("total");
+  const totalAmountDisplay = document.getElementById("total-amount");
+
+  let expenses = []
+});
