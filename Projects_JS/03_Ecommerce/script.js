5542c740c7eca51def5d2680cdc2631aa22dac09
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-06-27 07:39:32 +0530

    adding items

---
+++ Projects_JS/03_Ecommerce/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const products = [
+    { id: 1, name: "Notebook", price: 9.99 },
+    { id: 2, name: "Pen", price: 4.99 },
+    { id: 3, name: "Bag", price: 19.99 },
+  ];
+  const cart = []
+  const productList = document.getElementById("product-list");
+  const cartItems = document.getElementById("cart-items");
+  const emptyCartMessage = document.getElementById("empty-cart");
+  const cartTotalMessage = document.getElementById("cart-total");
+  const totalPriceDisplay = document.getElementById("total-price");
+  const checkOutBtn = document.getElementById("checkout-btn");
+
+});
