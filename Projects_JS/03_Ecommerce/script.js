759b649cb5ebe117ba12e39dbcf8aaf27a7ea6aa
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-06-27 13:32:19 +0530

    purchase complete

--- Projects_JS/03_Ecommerce/script.js
+++ Projects_JS/03_Ecommerce/script.js
@@ -4,7 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     { id: 2, name: "Pen", price: 4.99 },
     { id: 3, name: "Bag", price: 19.99 },
   ];
-  const cart = []
+
+  const cart = [];
+
   const productList = document.getElementById("product-list");
   const cartItems = document.getElementById("cart-items");
   const emptyCartMessage = document.getElementById("empty-cart");
@@ -12,4 +14,49 @@ document.addEventListener("DOMContentLoaded", () => {
   const totalPriceDisplay = document.getElementById("total-price");
   const checkOutBtn = document.getElementById("checkout-btn");
 
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    productDiv.innerHTML = `
+    <span>${product.name} - $${product.price.toFixed(2)}</span>
+    <button data-id="${product.id}">Add to cart</button>`;
+    productList.appendChild(productDiv);
+  });
+
+  productList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const productId = parseInt(e.target.getAttribute("data-id"));
+      const product = products.find((p) => p.id === productId);
+      addToCart(product);
+    }
+  });
+
+  function addToCart(product) {
+    cart.push(product);
+    renderCart();
+  }
+  function renderCart() {
+    cartItems.innerHTML = "";
+    let totalPrice = 0;
+
+    if (cart.length > 0) {
+      emptyCartMessage.classList.add("hidden");
+      cartTotalMessage.classList.remove("hidden");
+      cart.forEach((item, index) => {
+        totalPrice += item.price;
+        const cartItem = document.createElement("div");
+        cartItem.innerHTML = `${item.name} - $${item.price.toFixed(2)}`;
+        cartItems.appendChild(cartItem);
+        totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
+      });
+    } else {
+      emptyCartMessage.classList.remove("hidden");
+      totalPriceDisplay.textContent = `0.00`;
+    }
+  }
+  checkOutBtn.addEventListener("click", () => {
+    cart.length = 0;
+    alert("Checkout Sucessfully");
+    renderCart();
+  });
 });
