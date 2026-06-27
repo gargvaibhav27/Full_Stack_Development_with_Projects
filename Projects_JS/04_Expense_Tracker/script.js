58e10b9472d8c14f570c8742ab2debc1aafd24df
gargvaibhav27 <gargvaibhav2711@gmail.com>
2026-06-27 14:52:05 +0530

    expense complete

--- Projects_JS/04_Expense_Tracker/script.js
+++ Projects_JS/04_Expense_Tracker/script.js
@@ -6,5 +6,62 @@ document.addEventListener("DOMContentLoaded", () => {
   //const total = document.getElementById("total");
   const totalAmountDisplay = document.getElementById("total-amount");
 
-  let expenses = []
+  let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+  let totalAmount = calculateTotal();
+  renderExpenses();
+
+  expenseForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = expenseNameInput.value.trim();
+    const amount = parseFloat(expenseAmountInput.value.trim());
+
+    if (name !== "" && !isNaN(amount) && amount > 0) {
+      const newExpense = {
+        id: Date.now(),
+        name: name,
+        amount: amount,
+      };
+      expenses.push(newExpense);
+      saveExpensesTolocal();
+      renderExpenses();
+      updateTotal();
+
+      //Clear Input
+      expenseNameInput.value = "";
+      expenseAmountInput.value = "";
+    }
+  });
+  function renderExpenses() {
+    expenseList.innerHTML = "";
+    expenses.forEach((expense) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${expense.name} - $${expense.amount}
+        <button data-id="${expense.id}">Delete</button>`;
+      expenseList.appendChild(li);
+    });
+  }
+
+  function calculateTotal() {
+    return expenses.reduce((sum, expense) => sum + expense.amount, 0);
+  }
+
+  function saveExpensesTolocal() {
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
+
+  function updateTotal() {
+    totalAmount = calculateTotal();
+    totalAmountDisplay.textContent = totalAmount.toFixed(2);
+  }
+
+  expenseList.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+      const expenseId = parseInt(e.target.getAttribute("data-id"));
+      expenses = expenses.filter((expense) => expense.id !== expenseId);
+
+      saveExpensesTolocal();
+      renderExpenses();
+      updateTotal();
+    }
+  });
 });
